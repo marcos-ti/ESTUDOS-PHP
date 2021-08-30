e82fd119aabc8dca25ccda459231b86acd318ae6
Marcos <saoroque@hotmail.com>
2021-08-29 21:31:19 -0300

    Echo Print

---
+++ echo_print.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php</title>
+</head>
+<body>
+    <!-- Tag echo -->
+    <p><?php echo 'Construtor da linguagem que printa'; ?></p>
+    <!-- Tag print -->
+    <p><?php print ('Função da linguagem que printa'); ?></p>
+    <!-- Tag print exemplificando a função: 1 retorna que está correto, 0 é erro -->
+    <p><?php echo print ('Função da linguagem que printa:<br>'); ?></p>
+</body>
+</html>
