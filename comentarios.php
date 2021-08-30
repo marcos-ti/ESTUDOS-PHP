151848539c01d710a395965868d6cee142fa4343
Marcos <saoroque@hotmail.com>
2021-08-30 12:42:57 -0300

    Comentarios

---
+++ comentarios.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php</title>
+</head>
+<body>
+   
+
+    <?php 
+    // Comentario de uma linha em PHP
+    echo 'Construtor da linguagem que printa<br>';
+    # Comentario de uma linha em PHP, no estilo Shell
+     print ('Função da linguagem que printa<br>');
+    /* 
+        Comentario de multiplas
+        linhas em PHP
+    */
+     echo print ('Função da linguagem que printa:<hr>');
+
+    ?>
+</body>
+</html>
