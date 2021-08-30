6c741d944a0b6eba9c2e58d5fd2ae19ab643c67c
Marcos <saoroque@hotmail.com>
2021-08-30 18:21:26 -0300

    Concatenacao

---
+++ concatenacao.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php</title>
+</head>
+<body>
+  
+    <?php
+    //valores no codigo (hard code)
+    $nome = 'Marcos';
+    $idade = 43;
+        //operador . concatena
+        echo 'A idade de '. $nome .' é '. $idade .' anos.';
+        echo '<br>';
+        //aspas duplas
+        echo "A idade de $nome é  $idade  anos.";
+        echo '<br>';
+        //aspas simples transforma as variaveis em strings
+        echo 'A idade de $nome é  $idade  anos.';
+
+     ?>
+
+    
+
+</body>
+</html>
