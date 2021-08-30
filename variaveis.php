d819d9a8e86b7d200c3d553285b456a20492201e
Marcos <saoroque@hotmail.com>
2021-08-30 14:16:40 -0300

    Variaveis

---
+++ variaveis.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php</title>
+</head>
+<body>
+  
+    <?php
+        //string
+        $nome = 'Marcos Faria';
+        //int
+        $idade = 43;
+        //float
+        $peso = 95.5;
+        //boolean
+        $fumante_sn = false; //(true = 1) ou (false = vazio)
+
+    ?>
+        <h1>Ficha cadastral</h1>
+        <br>
+        <p>Nome: <?= $nome ?></p>
+        <p>Idade: <?= $idade ?></p>
+        <p>Peso: <?= $peso ?></p>
+        <p>Fumante: <?= $fumante_sn ?></p>
+
+    
+
+</body>
+</html>
