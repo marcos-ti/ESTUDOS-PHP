94822cdb3bf9e5e021b59afef65da35e6d6fcb07
Marcos <saoroque@hotmail.com>
2021-09-13 18:29:18 -0300

    Operadores-Aritmeticos

---
+++ operadores_aritmeticos.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php</title>
+</head>
+<body>
+   
+
+    <?php 
+        $num1 = 13;
+        $num2 = 4;
+
+        echo "O resultado entre $num1 + $num2 é:" . ' ' . ($num1 + $num2);
+        echo '<br>';
+        echo "O resultado entre $num1 - $num2 é:" . ' ' . ($num1 - $num2);
+        echo '<br>';
+        echo "O resultado entre $num1 * $num2 é:" . ' ' . ($num1 * $num2);
+        echo '<br>';
+        echo "O resultado entre $num1 / $num2 é:" . ' ' . ($num1 / $num2);
+        echo '<br>';
+        echo "O resultado entre $num1 % $num2 é:" . ' ' . ($num1 % $num2);
+        
+    ?>
+</body>
+</html>
